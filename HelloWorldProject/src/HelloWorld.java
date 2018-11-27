50d1b4cb4cd0f299bf8da5369743060307e98f29
paul.pomahac <paul.pomahac@KK-GYM-EDV1-11.gym.kk.local>
2018-11-27 17:48:03 +0100

    KOMMENTAR HINZU

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Klasse");	
+		System.out.println("Hallo Klasse");
+		// das ist eoin Kommentar, kein Code der ausgeführt wird
+		// das ist ein einzeiliger Kommentar
 	}
-
 }
