2b769127d96e603060ac0e881a2ab3db29bf2282
pomahac <45398022+pomahac@users.noreply.github.com>
2018-11-27 17:50:35 +0100

    Update HelloWorld.java

--- HelloWorldProject/src/HelloWorld.java
+++ HelloWorldProject/src/HelloWorld.java
@@ -3,8 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Klasse");
-		// das ist eoin Kommentar, kein Code der ausgeführt wird
+		System.out.println("Hallo PAUL");
+		// das ist eoin Kommentar, kein Code der ausgefÃ¼hrt wird
 		// das ist ein einzeiliger Kommentar
 	}
-}
+} 
