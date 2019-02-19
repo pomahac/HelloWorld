e30f067b86a7ca73f21809aef2bda7b16d7a2c07
paul.pomahac <paul.pomahac@KK-GYM-EDV1-11.gym.kk.local>
2019-02-19 17:09:29 +0100

    fdfdfdfd

---
+++ HelloWorldProject/src/sortierren/bubblesort.java
@@ -0,0 +1,33 @@
+package sortierren;
+
+public class bubblesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Länge der Reihe
+				int[] zahlen = { 17, 9, 44, 12, 99, 22, 51, 21, 7 };
+				System.out.println("Die Reihe hat " + zahlen.length + " Zahlen");
+				for (int i = 0; i < zahlen.length; i++) {
+					System.out.println(zahlen[i]);
+				}
+				//
+				System.out.println("----------------------");
+				//
+				// Bubblesort
+				for (int z = zahlen.length; z > 1; z--) {
+					for (int s = 0; s < z - 1; s++) {
+						if (zahlen[s] > zahlen[s + 1]) {
+							int a = zahlen[s];
+							zahlen[s] = zahlen[s + 1];
+							zahlen[s + 1] = a;
+
+						}
+					}
+				}
+				for (int i = 0; i < zahlen.length; i++) {
+					System.out.println(zahlen[i]);
+				}
+
+			}
+		}
