752453c3a2fec91a43640556b10a303370847bce
paul.pomahac <paul.pomahac@KK-GYM-EDV1-11.gym.kk.local>
2019-03-19 18:04:46 +0100

    vjzhg

--- HelloWorldProject/src/sortierren/quicksort.java
+++ HelloWorldProject/src/sortierren/quicksort.java
@@ -1,33 +1,50 @@
 package sortierren;
 
 public class quicksort {
-
-	//
-	static int[] zahlen = { 4, 2, 5, 9, 6, 10, 9, 7 };
-
-// 
 	public static void main(String[] args) {
-// unsortierte Zahlen ausgeben
-// sortieren
-// quicksort(0,6) // zahlen.lengh -1
-// sortierte Zahlen ausgeben
+		int a[]={2,5,6,8,7,3,4};
+		quicksort(a, 0, 6);
+		
+		for(int i==)
 	}
-
-static void quicksort(int i, int j) { 
-
-	//falls i < j dann 
-if(){
-int pivot = pivot(i,j);
-quicksort(i,pivot-1);
-quicksort(pivot+1,j):
+	public static void quicksort (int a [], int start, int end){
+if(start < end) {
+	int partitionIndex= partition, start ,end);
+	quicksort(a, start, partitionIndex-1,);
+	quicksort(a, start, partitionIndex+1,end);
+	
+	
 }
 
+	}
+public static int partition int a[], int start, int end){
+
+int piviot = a[end];
+int partitionIndex=start;
+for(int i=start; i<end; i++){
+if(a[i]<=pivot){
+	int temp= a[i];
+	a[i]=a[partitionIndex]
+			a[partitionIndex]=temp
+			partitionIndex=partitionIndex+1;
+}
 }
 
-private static int teile(int i,int j) { 
-	// variablen
-	int i;
-	int j;
-	//
-	do {
-f
+int temp1=a[partitionIndex];
+a[partitionIndex]=a[end];
+a[end]=temp1;
+return partition
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
