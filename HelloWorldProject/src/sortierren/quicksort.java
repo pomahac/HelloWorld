abcf026df02c83f1fc9d6df93637a08ee0ec139f
paul.pomahac <paul.pomahac@KK-GYM-EDV1-11.gym.kk.local>
2019-03-19 18:02:23 +0100

    .hhhhhhhhhh

--- HelloWorldProject/src/sortierren/quicksort.java
+++ HelloWorldProject/src/sortierren/quicksort.java
@@ -2,9 +2,32 @@ package sortierren;
 
 public class quicksort {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//
+	static int[] zahlen = { 4, 2, 5, 9, 6, 10, 9, 7 };
 
+// 
+	public static void main(String[] args) {
+// unsortierte Zahlen ausgeben
+// sortieren
+// quicksort(0,6) // zahlen.lengh -1
+// sortierte Zahlen ausgeben
 	}
 
+static void quicksort(int i, int j) { 
+
+	//falls i < j dann 
+if(){
+int pivot = pivot(i,j);
+quicksort(i,pivot-1);
+quicksort(pivot+1,j):
 }
+
+}
+
+private static int teile(int i,int j) { 
+	// variablen
+	int i;
+	int j;
+	//
+	do {
+f
